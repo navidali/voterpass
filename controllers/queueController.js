54d1afb1828638631711248070ce5652e65fc9dc
Navid <navidali@ufl.edu>
2020-07-12 14:39:44 -0400

    added local db

---
+++ controllers/queueController.js
@@ -0,0 +1,33 @@
+const PouchDB = require('pouchdb-browser');
+
+// PouchDB is viewable in dev tools 
+// Navigate to Application -> IndexedDB -> name of database
+var db = new PouchDB('sample database');
+
+// server db not configured yet
+var remoteCouch = false;
+
+// adds entry to db
+function add(){
+
+}
+
+// removes an entry from db
+function remove(){
+
+}
+
+// updates a single entry
+function update(){
+
+}
+
+// returns all db entries
+function listAll(){
+
+}
+
+// return one entry
+function find(){
+
+}
