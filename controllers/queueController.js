0f938995df90a29acae8f0f6a879803388e14b37
Navid <navidali@ufl.edu>
2020-07-15 05:04:45 -0400

    working demo

--- controllers/queueController.js
+++ controllers/queueController.js
@@ -3,21 +3,22 @@ PouchDB.plugin(require('pouchdb-find'));
 
 // PouchDB is viewable in dev tools 
 // Navigate to Application -> IndexedDB -> name of database
-var localDB = new PouchDB('mylocaldb');
-
-// server db not configured yet
-var remoteDB = new PouchDB('http://user:password@127.0.0.1:5984/database'); // example couchdb server http://user:password@127.0.0.1:5984/database_name
+const localDB = new PouchDB('mylocaldb');
+const remoteDB = new PouchDB('http://navidali:navid786@127.0.0.1:5984/myremotedb'); // example couchdb server http://user:password@127.0.0.1:5984/database_name
+let voters = [];
+let eta = 24;
 
 // adds entry to db
 function addVoter(name){
     var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     localDB.put({
-            _id: new Date().toISOString(),
+            _id: today.toISOString(),
             voterName: name,
-            time_enter: time
+            time_enter: today.toLocaleTimeString(),
+            time_return: new Date(new Date().getTime() + eta * 60000).toLocaleTimeString()   // manually setting 24 min ahead
     }).then(function(result) {
         console.log("new entry recorded");
+        eta += 24;
     }).catch(function(err) {
         console.log(err);
     });
@@ -25,21 +26,37 @@ function addVoter(name){
 
 // removes an entry from db
 function removeVoter(name){
-
-    localDB.createIndex({
-        index: {fields: ['voterName']}
-    });
-
-    localDB.find({
-        selector: {voterName: name},
-        fields: ['_id', 'voterName','time_enter', '_rev']
-    }).then(function (result) {
-        return localDB.remove(result.docs[0]);
-    }).then(function(result){
-        console.log("voter was removed from queue");
-    }).catch(function(err){
+    var ddoc = {
+        _id: '_design/index',
+        views: {
+          index: {
+            map: function mapFun(doc) {
+              if (doc.voterName) {
+                emit(doc.voterName);
+              }
+            }.toString()
+          }
+        }
+      }
+      
+      // save the design doc
+      localDB.put(ddoc).catch(function (err) {
+        if (err.name !== 'conflict') {
+          throw err;
+        }
+        // ignore if doc already exists
+      }).then(function () {
+        // find docs where title === 'Lisa Says'
+        return localDB.query('index', {
+          key: name,
+          include_docs: true
+        });
+      }).then(function (result) {
+        console.log("removed doc");
+        return localDB.remove(result.rows[0].doc);
+      }).catch(function (err) {
         console.log(err);
-    });
+      });
 }
 
 // updates name of entry 
@@ -51,7 +68,7 @@ function updateVoterName(oldName, newName){
 
     localDB.find({
         selector: {voterName: oldName},
-        fields: ['_id', 'voterName','time_enter', '_rev']
+        fields: ['_id', 'voterName','time_enter', 'time_return', '_rev']
     }).then(function (result) {
         return localDB.put({
            _id: result.docs[0]._id,
@@ -67,12 +84,80 @@ function updateVoterName(oldName, newName){
 }
 
 // returns all db entries
-function listAllVoters(){
+function setVoters(){
     localDB.allDocs({
         include_docs: true,
+        startkey: "9",
+        endkey: "0",
         descending: true
-    }).then(function(result) {
-        console.log(result);
+    }).then(function (result) {
+        voters = result.rows;
+        console.log(result.rows);
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function getVoters(){
+    setVoters();
+    return voters;
+}
+
+function remove(name){
+    var ddoc = {
+        _id: '_design/index',
+        views: {
+          index: {
+            map: function mapFun(doc) {
+              if (doc.voterName) {
+                emit(doc.voterName);
+              }
+            }.toString()
+          }
+        }
+      }
+      
+      // save the design doc
+      localDB.put(ddoc).catch(function (err) {
+        if (err.name !== 'conflict') {
+          throw err;
+        }
+        // ignore if doc already exists
+      }).then(function () {
+        // find docs where title === 'Lisa Says'
+        return localDB.query('index', {
+          key: name,
+          include_docs: true
+        });
+      }).then(function (result) {
+        console.log(result.rows[0].doc);
+        return localDB.remove(result.rows[0].doc);
+      }).catch(function (err) {
+        console.log(err);
+      });
+}
+
+function drawTable() {
+    const tbody = document.querySelector("tbody");
+    localDB.allDocs({
+        include_docs: true,
+        startkey: "0",
+        endkey: "9",
+        //descending: true
+    }).then(function (result) {
+        if (result.rows.length != 0){
+            result.rows.forEach( voter => {
+                let row = tbody.insertRow();
+                let name = row.insertCell(0);
+                name.innerHTML = voter.doc.voterName;
+                let time_enter = row.insertCell(1);
+                time_enter.innerHTML = voter.doc.time_enter;
+                let time_return = row.insertCell(2);
+                time_return.innerHTML = voter.doc.time_return;
+            
+            });
+        }
+        voter = result.rows;
     }).catch(function (err) {
         console.log(err);
     });
